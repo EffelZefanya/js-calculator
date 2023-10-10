b37cfb90e6398382b9f401dbe014430fc9d3cdca
Fel <99955631+EffelZefanya@users.noreply.github.com>
2023-10-10 23:09:01 +0700

    feat: add number, operator and decimal button works

---
+++ script.js
@@ -0,0 +1,44 @@
+let topScreen = document.querySelector("div[previous-operand]");
+let bottomScreen = document.querySelector("div[current-operand]");
+let numberButtons = document.querySelectorAll("button[number-button]");
+let operationButtons = document.querySelectorAll("button[operation-button]");
+let decimalButton = document.querySelector("button[decimal-button]");
+let clearButton = document.querySelector("button[clear-button]");
+let deleteButton = document.querySelector("button[delete-button]");
+let equalsButton = document.querySelector("button[equals-button]");
+
+bottomScreen.textContent = "0";
+topScreen.textContent = "";
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (bottomScreen.textContent === "0") {
+      bottomScreen.textContent = button.textContent;
+    } else {
+      bottomScreen.textContent += button.textContent;
+    }
+  });
+});
+
+decimalButton.addEventListener("click", () => {
+  if (!bottomScreen.textContent.includes(".")) {
+    bottomScreen.textContent += ".";
+  }
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (topScreen.textContent !== "") {
+      equalsButton.click();
+    }
+    topScreen.textContent = bottomScreen.textContent + button.textContent;
+    bottomScreen.textContent = "";
+  });
+});
+
+equalsButton.addEventListener("click", () => {
+  if (topScreen.textContent !== "") {
+    topScreen.textContent = topScreen.textContent + bottomScreen.textContent;
+    bottomScreen.textContent = eval(topScreen.textContent);
+  }
+});
